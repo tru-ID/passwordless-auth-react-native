d9135ce7375b23239ef0bbaff9c1b24c13f1797e
glamboyosa <ogbemudiatimothy@gmail.com>
2021-07-28 13:45:44 +0100

    chore: update `.prettierrc.js`

--- App.js
+++ App.js
@@ -16,9 +16,10 @@ import {
   TextInput,
   TouchableOpacity,
   ActivityIndicator,
+  Image,
 } from 'react-native';
 import {Dimensions} from 'react-native';
-import LinearGradient from 'react-native-linear-gradient'
+import LinearGradient from 'react-native-linear-gradient';
 import FlashMessage, {showMessage} from 'react-native-flash-message';
 
 const App = () => {
@@ -29,52 +30,51 @@ const App = () => {
     <>
       <StatusBar barStyle="light-content" />
       <LinearGradient
-      colors={['rgba(25, 85, 255, 40)', 'rgba(10, 10, 50, 66)']}
-      useAngle={true}
-      angle={0}
-      style={{
-        flex: 1,
-      }}
-    >
+        colors={['rgba(25, 85, 255, 40)', 'rgba(10, 10, 50, 66)']}
+        useAngle={true}
+        angle={0}
+        style={{
+          flex: 1,
+        }}>
         <FlashMessage />
       </LinearGradient>
     </>
   );
 };
 
- const styles = StyleSheet.create({
-   container: {
+const styles = StyleSheet.create({
+  container: {
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-   },
-   box: {
+  },
+  box: {
     width: '90%',
     borderRadius: 3,
     backgroundColor: '#fff',
     elevation: 5,
     shadowColor: '#000',
-    shadowOffset: { width: 0.5, height: 1 },
+    shadowOffset: {width: 0.5, height: 1},
     shadowOpacity: 0.8,
     shadowRadius: 1,
     justifyContent: 'center',
     alignItems: 'center',
     height: 0.7 * Dimensions.get('window').height,
     padding: 15,
-   },
-   toastContainer: {
-     flex: 1,
-     justifyContent: 'center',
-     alignItems: 'center',
-   },
-   heading: {
-     fontSize: 0.1 * Dimensions.get('window').width,
-   },
-   paragraph: {
-     fontSize: 20,
-    marginBottom: 15
-   },
-   spinner: {
+  },
+  toastContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  heading: {
+    fontSize: 0.1 * Dimensions.get('window').width,
+  },
+  paragraph: {
+    fontSize: 20,
+    marginBottom: 15,
+  },
+  spinner: {
     marginTop: 20,
   },
   button: {
@@ -93,7 +93,7 @@ const App = () => {
   buttonText: {
     color: '#fff',
   },
-   textInput: {
+  textInput: {
     padding: 15,
     borderRadius: 3,
     backgroundColor: '#fff',
@@ -101,12 +101,12 @@ const App = () => {
     borderWidth: 0.4,
     elevation: 7,
     shadowColor: '#858585',
-    shadowOffset: { width: 0.5, height: 1 },
+    shadowOffset: {width: 0.5, height: 1},
     shadowOpacity: 0.8,
     shadowRadius: 1,
     color: '#000',
     width: 0.7 * Dimensions.get('window').width,
-   },
- });
+  },
+});
 
 export default App;
